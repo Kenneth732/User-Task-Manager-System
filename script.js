a8bdd214dd00ee85768d8c3bade527082fca58e3
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-13 13:26:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let taskList = [];
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let name = e.target.name.value;
+    let img = e.target.img.value;
+})
